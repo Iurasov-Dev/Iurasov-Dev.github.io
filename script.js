3f9187c501a22243ee014aee7e3d3fcfe00980c9
Viacheslav Iurasov <v.iurasov@mail.ru>
2024-10-11 21:45:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -10,41 +10,47 @@ function cancel() {
     document.body.style.overflow = "auto"; // Восстанавливаем прокрутку
 }
 
-// Typewriter Effect
-const texts = [
-    "DATA SCIENTIST",
-    "DESIGNER",
-    "DEVELOPER",
-    "SEAFARER",
-    "TILTED HORIZON"
-];
+document.addEventListener('DOMContentLoaded', () => {
+    // Typewriter Effect
+    const texts = [
+        "DATA SCIENTIST",
+        "DESIGNER",
+        "DEVELOPER",
+        "SEAFARER",
+        "TILTED HORIZON"
+    ];
 
-let speed = 100;
-const textElements = document.querySelector(".typewriter-text");
+    let speed = 100;
+    const textElements = document.querySelector(".typewriter-text");
 
-let textIndex = 0;
-let charcterIndex = 0;
+    let textIndex = 0;
+    let charcterIndex = 0;
 
-function typeWriter() {
-    if (charcterIndex < texts[textIndex].length) {
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed);
-    } else {
-        setTimeout(eraseText, 1000);
-    }
-}
+    const typeWriter = () => {
+        if (charcterIndex < texts[textIndex].length) {
+            textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+            charcterIndex++;
+            setTimeout(typeWriter, speed);
+        } else {
+            setTimeout(eraseText, 1000);
+        }
+    };
 
-function eraseText() {
-    if (textElements.innerHTML.length > 0) {
-        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
-        setTimeout(eraseText, 50);
-    } else {
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500);
-    }
-}
+    const eraseText = () => {
+        if (textElements.innerHTML.length > 0) {
+            textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+            setTimeout(eraseText, 50);
+        } else {
+            textIndex = (textIndex + 1) % texts.length;
+            charcterIndex = 0;
+            setTimeout(typeWriter, 500);
+        }
+    };
+
+    // Запускаем эффект печати
+    typeWriter();
+});
+
 
 function toggleSections() {
     const aboutSection = document.querySelector('#about');
