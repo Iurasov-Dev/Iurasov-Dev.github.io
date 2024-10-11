7976a0c1d0eb28eaa0367cad5afcde36e5fa6488
Viacheslav Iurasov <v.iurasov@mail.ru>
2024-10-11 21:52:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -50,17 +50,6 @@ function eraseText(){
 
 window.onload = typeWriter
 
-// Функции для работы с меню
-function hamburg() {
-    const navbar = document.querySelector(".dropdown");
-    navbar.style.transform = "translateY(0px)";
-}
-
-function cancel() {
-    const navbar = document.querySelector(".dropdown");
-    navbar.style.transform = "translateY(-500px)";
-}
-
 
 function toggleSections() {
     const aboutSection = document.querySelector('#about');
