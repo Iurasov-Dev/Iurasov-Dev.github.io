c8044f7177371b61db585a4a38b22a0b330e0f0d
Viacheslav Iurasov <v.iurasov@mail.ru>
2026-07-05 01:55:12 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ function cancel() {
 
 // Typewriter Effect
 const texts = [
-    "DATA SCIENTIST",
-    "DESIGNER",
-    "DEVELOPER",
-    "SEAFARER",
-    "TILTED HORIZON"
+    "DATA SCIENTIST 👨‍💻",
+    "DESIGNER 👨‍🏭",
+    "DEVELOPER 👷",
+    "SEAFARER 👮",
+    "PHOTHOGRAPHER 👨‍💼"
 ];
 
 let speed = 100; // Speed of typing effect
